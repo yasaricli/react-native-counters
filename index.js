4a633cb6df585fd2430c430bd1ca71db80586193
Yaşar İÇLİ <yasaricli@gmail.com>
2018-09-28 10:30:19 +0300

    prettier standart.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { Text, View, TouchableOpacity, StyleSheet } from "react-native";
-import PropTypes from 'prop-types';
+import PropTypes from "prop-types";
 
 export default class Counter extends Component {
   constructor(props) {
@@ -17,56 +17,108 @@ export default class Counter extends Component {
 
   onPressMinus() {
     const { number } = this.state;
-    const minusNumber = (number - 1);
+    const minusNumber = number - 1;
 
     if (number == this.props.min) {
-      return
+      return;
     }
 
-    return this.setState({ number: minusNumber }, () => this.props.onChange(minusNumber, '-'))
+    return this.setState({ number: minusNumber }, () =>
+      this.props.onChange(minusNumber, "-")
+    );
   }
 
   onPressPlus() {
     const { number } = this.state;
-    const plusNumber = (number + 1);
+    const plusNumber = number + 1;
 
     if (number == this.props.max) {
-      return
+      return;
     }
 
-    return this.setState({ number: plusNumber }, () => this.props.onChange(plusNumber, '+'))
+    return this.setState({ number: plusNumber }, () =>
+      this.props.onChange(plusNumber, "+")
+    );
   }
 
   renderMinusButton() {
-    const { min, touchableDisabledColor, touchableColor, minusIcon } = this.props;
+    const {
+      min,
+      touchableDisabledColor,
+      touchableColor,
+      minusIcon
+    } = this.props;
     const isMinusDisabled = min == this.state.number;
     const buttonStyle = {
       borderColor: isMinusDisabled ? touchableDisabledColor : touchableColor
     };
 
     return (
-      <TouchableOpacity style={[Styles.touchable, buttonStyle ]} onPress={this.onPressMinus} activeOpacity={isMinusDisabled ? 0.9 : 0.2}>
-        {this.props.minusIcon ? this.props.minusIcon(isMinusDisabled, touchableDisabledColor, touchableColor) :
-          <Text style={[Styles.iconText, { color: isMinusDisabled ? touchableDisabledColor : touchableColor }]}>-</Text>
-        }
+      <TouchableOpacity
+        style={[Styles.touchable, buttonStyle]}
+        onPress={this.onPressMinus}
+        activeOpacity={isMinusDisabled ? 0.9 : 0.2}
+      >
+        {this.props.minusIcon ? (
+          this.props.minusIcon(
+            isMinusDisabled,
+            touchableDisabledColor,
+            touchableColor
+          )
+        ) : (
+          <Text
+            style={[
+              Styles.iconText,
+              {
+                color: isMinusDisabled ? touchableDisabledColor : touchableColor
+              }
+            ]}
+          >
+            -
+          </Text>
+        )}
       </TouchableOpacity>
-    )
+    );
   }
 
   renderPlusButton() {
-    const { max, touchableDisabledColor, touchableColor, plusIcon } = this.props;
+    const {
+      max,
+      touchableDisabledColor,
+      touchableColor,
+      plusIcon
+    } = this.props;
     const isPlusDisabled = max == this.state.number;
     const buttonStyle = {
       borderColor: isPlusDisabled ? touchableDisabledColor : touchableColor
     };
 
     return (
-      <TouchableOpacity style={[Styles.touchable, buttonStyle ]} onPress={this.onPressPlus} activeOpacity={isPlusDisabled ? 0.9 : 0.2}>
-        {this.props.plusIcon ? this.props.plusIcon(isPlusDisabled, touchableDisabledColor, touchableColor) :
-          <Text style={[Styles.iconText, { color: isPlusDisabled ? touchableDisabledColor : touchableColor }]}>+</Text>
-        }
+      <TouchableOpacity
+        style={[Styles.touchable, buttonStyle]}
+        onPress={this.onPressPlus}
+        activeOpacity={isPlusDisabled ? 0.9 : 0.2}
+      >
+        {this.props.plusIcon ? (
+          this.props.plusIcon(
+            isPlusDisabled,
+            touchableDisabledColor,
+            touchableColor
+          )
+        ) : (
+          <Text
+            style={[
+              Styles.iconText,
+              {
+                color: isPlusDisabled ? touchableDisabledColor : touchableColor
+              }
+            ]}
+          >
+            +
+          </Text>
+        )}
       </TouchableOpacity>
-    )
+    );
   }
 
   render() {
@@ -74,31 +126,27 @@ export default class Counter extends Component {
 
     return (
       <View style={Styles.container}>
-        <View>
-          {this.renderMinusButton()}
-        </View>
+        <View>{this.renderMinusButton()}</View>
         <View style={Styles.number}>
-          <Text style={[Styles.text, { color: this.props.textColor }]}>
+          <Text style={[Styles.text, { color: this.props.textColor }]}>
             {number}
           </Text>
         </View>
-        <View>
-          {this.renderPlusButton()}
-        </View>
+        <View>{this.renderPlusButton()}</View>
       </View>
-    )
+    );
   }
 }
 
 const Styles = StyleSheet.create({
   container: {
-    flexDirection: 'row',
+    flexDirection: "row"
   },
 
   text: {
     fontSize: 16,
     paddingLeft: 15,
-    paddingRight: 15,
+    paddingRight: 15
   },
 
   iconText: {
@@ -144,9 +192,9 @@ Counter.defaultProps = {
     // Number, - or +
   },
 
-  textColor: '#196583',
-  touchableColor: '#27AAE1',
-  touchableDisabledColor: '#B5E9FF',
+  textColor: "#196583",
+  touchableColor: "#27AAE1",
+  touchableDisabledColor: "#B5E9FF",
 
   minusIcon: null,
   plusIcon: null
